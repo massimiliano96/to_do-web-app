cc9577bff462313f35a0e50ddc226c889a230047
Massi <massimilianoriva@icloud.com>
2022-07-09 15:30:55 +0200

    first commit : base todo_item and to_do_factory added

---
+++ src/to_do/structs/mod.rs
@@ -0,0 +1,5 @@
+//this enables other files within the module to access the base file
+//we don't make it public since we don't want base file to be used outside the module
+mod base;
+pub mod done;
+pub mod pending;
