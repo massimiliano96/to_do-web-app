2cfe576bdce0ee3fe180ad248bb41ba8e504a4c4
massimiliano96 <48362794+massimiliano96@users.noreply.github.com>
2022-07-13 08:43:55 +0200

    Handling http requests and Views module
    * actix web-server and first commit of views module
    
    * views factories and URL Path struct
    
    Co-authored-by: Massimiliano Riva <m.riva@crif.com>

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,13 @@
-mod to_do;
-mod state;
-mod processes;
+use actix_web::{App, HttpServer};
+mod views;
 
-use std::env;
-use state::read_file;
-use serde_json::value::Value;
-use serde_json::Map;
-use to_do::to_do_factory;
-use processes::process_input;
-
-fn main() {
-    let args : Vec<String> = env::args().collect();
-    let command: &String = &args[1];
-    let title : &String = &args[2];
-
-    let mut state: Map<String, Value> =read_file("./state.json");
-
-    let status: String;
-
-    match &state.get(*&title) {
-        Some(result) => {
-            status = result.to_string().replace('\"', "");
-        }
-        None => {
-            status = String::from("pending");
-        }
-    }
-    let item = to_do_factory(&status, title).expect(&status);
-    process_input(item, command.to_string(), &mut state);
+#[actix_rt::main]
+async fn main()  -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let app = App::new().configure(views::views_factory );
+        return app
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
