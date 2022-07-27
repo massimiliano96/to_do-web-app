5c872044628143a499861ee93335e2680eb1bfbd
Massimiliano Riva <m.riva@crif.com>
2022-07-27 18:07:06 +0200

    in progress

--- src/views/app/items.rs
+++ src/views/app/items.rs
@@ -2,7 +2,11 @@ use actix_web::HttpResponse;
 use super::content_loader::read_file;
 
 pub async fn items() -> HttpResponse {
-    let html_data = read_file("./templates/main.html");
+    let mut html_data = read_file("./templates/main.html");
+    let javascript_data = read_file("./javascript/main.js");
+
+    html_data = html_data.replace("{{JAVASCRIPT}}", &javascript_data);
+
     HttpResponse::Ok()
         .content_type("text/html; charset=uft-8")
         .body(html_data)
--- src/views/to_do/mod.rs
+++ src/views/to_do/mod.rs
@@ -9,8 +9,9 @@ use super::path::Path;
 
 pub fn item_factory(app: &mut web::ServiceConfig) {
     let base_path: Path = Path{prefix: String::from("/item")};
+    println!("Item Factory called");
     app.route(&base_path.define(String::from("/create/{title}")), web::post().to(create::create));
     app.route(&base_path.define(String::from("/get")), web::get().to(get::get));
     app.route(&base_path.define(String::from("/edit")), web::put().to(edit::edit));
-    app.route(&base_path.define(String::from("/delete/{title}")), web::post().to(delete::delete));
+    app.route(&base_path.define(String::from("/delete")), web::post().to(delete::delete));
 }
