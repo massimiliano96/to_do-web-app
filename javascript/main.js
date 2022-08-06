06703339998ae4b31f6dc5cc9627098dfb81c338
Massi <massimilianoriva@icloud.com>
2022-08-06 14:02:00 +0200

    Header component injection

--- javascript/main.js
+++ javascript/main.js
@@ -25,8 +25,9 @@ function apiCall(url, method) {
             console.log("pre parse : " + this.responseText);
             renderItems(JSON.parse(this.responseText)["pending_items"], "edit", "pendingItems", editItem);
             renderItems(JSON.parse(this.responseText)["done_items"], "delete", "doneItems", deleteItem);
-            document.getElementById("completeNum").innerHTML = JSON.parse(this.responseText)["done_item_count"];
-            document.getElementById("pendingNum").innerHTML = JSON.parse(this.responseText)["pending_item_count"];
+            document.getElementById("completeNum").innerHTML = JSON.parse(this.responseText)["done_items_count"];
+            document.getElementById("pendingNum").innerHTML = JSON.parse(this.responseText)["pending_items_count"];
+
             console.log("post parse");
         }
     });
