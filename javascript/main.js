c9f8d15d590e1ef6a4e9ee268e4bc4c94ea57124
massimiliano96 <48362794+massimiliano96@users.noreply.github.com>
2022-08-06 14:02:43 +0200

    Displaying content browser
    * in progress
    
    * fix Create view
    
    * Fix rendering items
    
    * Injecting CSS
    
    * CSS HTML JS injection
    
    * Header component injection
    
    Co-authored-by: Massimiliano Riva <m.riva@crif.com>
    Co-authored-by: Massi <massimilianoriva@icloud.com>

---
+++ javascript/main.js
@@ -0,0 +1,78 @@
+function renderItems(items, processType, elementId, processFunction) {
+    let placeholder = "<div>";
+    let itemsMeta = []; //we place the items title in order to add listener later on
+    for(i = 0; i < items.length; i++) {
+        let title = items[i]["title"];
+        let placeholderId = processType + "-" + title.replace(" ", "-");
+        placeholder += '<div class="itemContainer">' + '<p>' + title + '</p>' + '<div class="actionButton" ' + 'id="' + placeholderId + '">'
+                        + processType + '</div>' + "</div>";
+        itemsMeta.push({"id": placeholderId, "title": title});
+    }
+    placeholder += "</div>";
+    document.getElementById(elementId).innerHTML = placeholder;
+
+    for (i = 0; i < itemsMeta.length; i++) {
+        document.getElementById(itemsMeta[i]["id"]).addEventListener("click", processFunction);
+    }
+    console.log("renderItems ends");
+}
+
+function apiCall(url, method) {
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener('readystatechange', function () {
+        if (this.readyState == this.DONE) {
+            console.log("pre parse : " + this.responseText);
+            renderItems(JSON.parse(this.responseText)["pending_items"], "edit", "pendingItems", editItem);
+            renderItems(JSON.parse(this.responseText)["done_items"], "delete", "doneItems", deleteItem);
+            document.getElementById("completeNum").innerHTML = JSON.parse(this.responseText)["done_items_count"];
+            document.getElementById("pendingNum").innerHTML = JSON.parse(this.responseText)["pending_items_count"];
+
+            console.log("post parse");
+        }
+    });
+    xhr.open(method, url);
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.setRequestHeader('user-token', 'token');
+    return xhr
+}
+
+function editItem() {
+    let title = this.id.replaceAll("-", " ").replace("edit ", "");
+    let call = apiCall("/item/edit", "PUT");
+    let json = {
+        "title": title,
+        "status": "done"
+    };
+    console.log("Editing Items: " + JSON.stringify(json));
+    call.send(JSON.stringify(json));
+}
+
+function deleteItem() {
+    let title = this.id.replaceAll("-", " ").replace("delete ", "");
+    let call = apiCall("/item/delete", "POST");
+    let json = {
+        "title": title,
+        "status": "done"
+    };
+    console.log("Deleting Item: " + JSON.stringify(json));
+    call.send(JSON.stringify(json));
+}
+
+function getItems() {
+    console.log("Getting Items");
+    let call = apiCall("/item/get", 'GET');
+    call.send();
+}
+
+getItems();
+
+document.getElementById("create-button").addEventListener("click", createItem);
+
+function createItem() {
+    let title = document.getElementById("name");
+    let call = apiCall("/item/create/" + title.value, 'POST');
+    console.log("Creating Item: "+ title.value);
+    call.send();
+    document.getElementById("name").value = null;
+}
--- src/views/to_do/create.rs
+++ src/views/to_do/create.rs
@@ -1,19 +1,21 @@
 use serde_json::value::Value;
 use serde_json::Map;
-use actix_web::HttpRequest;
+use actix_web::{HttpRequest, Responder};
 
 use crate::to_do;
 use crate::state::read_file;
 use crate::processes::process_input;
+use super::utils::return_state;
 
-pub async fn create(req: HttpRequest) -> String {
+pub async fn create(req: HttpRequest) -> impl Responder {
     let mut state: Map<String, Value> = read_file("./state.json");
 
     let title: String = req.match_info().get("title").unwrap().to_string();
-    let title_reference : String = title.clone();
 
     let item = to_do::to_do_factory(&String::from("pending"), &title).expect("create ");
+
+    // add the to_do item to state.json
     process_input(item, String::from("create"), &mut state);
 
-    return format!("{} created", title_reference)
+    return return_state()
 }
